a08ff3616c3a2d440dc5b08e06692558e4bdda3e
captcha781 <bhuvanesh19112001@gmail.com>
2023-01-09 12:31:39 +0530

    fixed order matching conditions

--- jobs/spotCron.js
+++ jobs/spotCron.js
@@ -5,10 +5,11 @@ const graphModel = require("../models/graphHistory")
 module.exports = () => {
     cron.schedule("*/1 * * * * *", async () => {
         const asset = await assetModel.findOne({ symbol: "BNB" })
-        const upPrice = Math.floor(Math.random() * 2)
-        const sign = Math.round(Math.random() * 1) === 1 ? "+" : "-"
-        await assetModel.findOneAndUpdate({ symbol: "BNB" }, { $set: { assetPrice: asset.assetPrice + Number(sign + upPrice) } })
-        await graphModel.create({symbol: "BNB", price: asset.assetPrice + Number(sign + upPrice)})
-        console.log("spot price cron", "Price: ",asset.assetPrice + Number(sign + upPrice));
+        //     const upPrice = Math.floor(Math.random() * 2)
+        //     const sign = Math.round(Math.random() * 1) === 1 ? "+" : "-"
+        //     await assetModel.findOneAndUpdate({ symbol: "BNB" }, { $set: { assetPrice: asset.assetPrice + Number(sign + upPrice) } })
+        //     await graphModel.create({symbol: "BNB", price: asset.assetPrice + Number(sign + upPrice)})
+        // console.log("spot price cron", "Price: ",asset.assetPrice + Number(sign + upPrice));
+        console.log("Spot price : " + asset.assetPrice);
     })
 }
